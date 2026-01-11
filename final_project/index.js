5d3c1acea0aa32b986759494fed1b52394788332
vaishpattu <vaishpattu@gmail.com>
2026-01-11 02:42:16 -0500

    Add user authentication for customer login
    Implement authentication mechanism for customer routes.

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+const authenticatedUser = require('./router/auth_users.js').authenticatedUser;
 const app = express();
 
 app.use(express.json());
@@ -12,6 +13,29 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    //next();
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        req.user = {username};
+        //return res.status(200).send("User successfully logged in");
+        next();
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
  
 const PORT =5000;
@@ -20,3 +44,4 @@ app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
+
